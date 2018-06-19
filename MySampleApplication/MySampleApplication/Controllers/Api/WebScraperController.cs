a2518c713736a28ca80c0a9ebb67a361e4108058
Isaac Briones <birdman12irb@gmail.com>
2018-06-18 18:07:05 -0700

    web scrape in process

---
+++ MySampleApplication/MySampleApplication/Controllers/Api/WebScraperController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using MySampleApplication.Helpers;
+using MySampleApplication.Models.Request;
+using MySampleApplication.Models.Responses;
+
+namespace MySampleApplication.Controllers.Api
+{
+    [RoutePrefix("api/webscraper")]
+    public class WebScraperController : ApiController
+    {
+        [HttpPost,AllowAnonymous]
+        public HttpResponseMessage GetData(WebScrapeAddRequest model)
+        {
+            WebScraper scraper = new WebScraper(model.Url);
+            ItemResponse<string> resp = new ItemResponse<string>();
+            resp.Item = scraper.GetContent("plan");
+            return Request.CreateResponse(System.Net.HttpStatusCode.OK, resp);
+        }
+    }
+}
---
+++ MySampleApplication/MySampleApplication/Helpers/WebScraper.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using HtmlAgilityPack;
+
+namespace MySampleApplication.Helpers
+{
+    public class WebScraper
+    {
+        public bool IsPageLoaded = false;
+        public string GetUrl { get; set; }
+
+        private HtmlDocument _htmlDocument;
+
+        public string GetContent(string propName)
+        {
+            HtmlWeb htmlWeb = new HtmlWeb();
+            HtmlDocument htmlDocument = htmlWeb.Load(GetUrl);
+
+            if (!this.IsPageLoaded)
+            {
+                this.loadHtml();
+            }
+
+            HtmlNode tags = _htmlDocument.DocumentNode.SelectSingleNode("//div");
+            foreach (HtmlNode tag in tags.SelectNodes("//figure/a"))
+            {
+                var test = tag.SelectSingleNode("//source");
+                var um = test.Attributes["srcset"].Value;
+                
+               
+            }
+            string content = string.Empty;
+            //foreach (var item in tags)
+            //{
+            //    string prop = item.GetAttributeValue("class","");
+            //    if (!string.IsNullOrWhiteSpace(prop) && prop == propName)
+            //    {
+            //        content = item.GetAttributeValue("content", "");
+            //        if (!string.IsNullOrEmpty(content))
+            //        {
+            //            break;
+            //        }
+            //    }
+            //}
+            return content;
+
+
+        }
+        private void loadHtml()
+        {
+            HtmlWeb htmlweb = new HtmlWeb();
+            _htmlDocument = htmlweb.Load(GetUrl);
+            IsPageLoaded = true;
+        }
+        public WebScraper()
+            {
+
+            }
+        public WebScraper(string url)
+        {
+
+            this.GetUrl = url;
+        }
+    }
+}
