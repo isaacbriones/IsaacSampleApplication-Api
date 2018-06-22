33a383ec694a8df84efe0fd4c136c8124b17abcd
Isaac Briones <birdman12irb@gmail.com>
2018-06-21 17:45:13 -0700

    supplement crud finished

---
+++ MySampleApplication/MySampleApplication.Services/SupplementService.cs
@@ -0,0 +1,172 @@
+﻿using System.Collections.Generic;
+using System.Data.SqlClient;
+using MySampleApplication.Models.Request;
+
+namespace MySampleApplication.Services
+{
+    public class SupplementService
+    {
+        public int? Insert(SupplementAddRequest model)
+        {
+            string connStr = System.Configuration.ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
+            using (SqlConnection conn = new SqlConnection(connStr))
+            {
+                conn.Open();
+                if (conn.State == System.Data.ConnectionState.Open)
+                {
+                    string sqlCmd = "Supplemnt_Insert";
+
+                    using (SqlCommand cmd = new SqlCommand(sqlCmd, conn))
+                    {
+
+                        cmd.CommandType = System.Data.CommandType.StoredProcedure;
+                        SqlParameter parm = new SqlParameter();
+                        parm.SqlDbType = System.Data.SqlDbType.Int;
+                        parm.Direction = System.Data.ParameterDirection.Output;
+                        parm.ParameterName = "@Id";
+
+                        cmd.Parameters.Add(parm);
+                        cmd.Parameters.AddWithValue("@SupplementName", model.SupplementName);
+                        cmd.Parameters.AddWithValue("@Brand", model.Brand);
+                        cmd.Parameters.AddWithValue("@Description", model.Description);
+                        cmd.Parameters.AddWithValue("@Price", model.Price);
+
+                        cmd.ExecuteNonQuery();
+
+                        int id = (int)cmd.Parameters["@Id"].Value;
+
+                        return id;
+                    }
+                }
+                else
+                {
+                    return null;
+                }
+            }
+        }
+
+        public void Update(SupplementAddRequest model)
+        {
+            string connStr = System.Configuration.ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
+            using (SqlConnection conn = new SqlConnection(connStr))
+            {
+                conn.Open();
+                if (conn.State == System.Data.ConnectionState.Open)
+                {
+                    string sqlCmd = "Supplemnt_Update";
+
+                    using (SqlCommand cmd = new SqlCommand(sqlCmd, conn))
+                    {
+
+                        cmd.CommandType = System.Data.CommandType.StoredProcedure;
+                        cmd.Parameters.AddWithValue("@Id", model.Id);
+                        cmd.Parameters.AddWithValue("@SupplementName", model.SupplementName);
+                        cmd.Parameters.AddWithValue("@Brand", model.Brand);
+                        cmd.Parameters.AddWithValue("@Description", model.Description);
+                        cmd.Parameters.AddWithValue("@Price", model.Price);
+
+                        cmd.ExecuteNonQuery();
+                    }
+                }
+            }
+        }
+
+        public SupplementAddRequest SelectById(int id)
+        {
+            SupplementAddRequest viewModel = new SupplementAddRequest();
+            string connStr = System.Configuration.ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
+            using (SqlConnection conn = new SqlConnection(connStr))
+            {
+                conn.Open();
+                if (conn.State == System.Data.ConnectionState.Open)
+                {
+                    string sqlCmd = "Supplemnt_Insert";
+
+                    using (SqlCommand cmd = new SqlCommand(sqlCmd, conn))
+                    {
+                        cmd.CommandType = System.Data.CommandType.StoredProcedure;
+                        cmd.Parameters.AddWithValue("@Id", model.Id);
+                        SqlDataReader reader = cmd.ExecuteReader(System.Data.CommandBehavior.CloseConnection);
+                        if (reader.Read())
+                        {
+                            int index = 0;
+                            viewModel.Id = reader.GetInt32(index++);
+                            viewModel.SupplementName = reader.GetString(index++);
+                            viewModel.Brand = reader.GetString(index++);
+                            viewModel.Description = reader.GetString(index++);
+                            viewModel.Price = reader.GetString(index++);
+
+                            return viewModel;
+                        }
+                        else
+                        {
+                            return null;
+                        }
+                    }
+                }
+                else
+                {
+                    return null;
+                }
+            }
+        }
+
+        public List<SupplementAddRequest> SelectAll()
+        {
+            List<SupplementAddRequest> result = new List<SupplementAddRequest>();
+            string connStr = System.Configuration.ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
+            using (SqlConnection conn = new SqlConnection(connStr))
+            {
+                conn.Open();
+                if (conn.State == System.Data.ConnectionState.Open)
+                {
+                    string sqlCmd = "Blog_GetAll";
+                    using (SqlCommand cmd = new SqlCommand(sqlCmd, conn))
+                    {
+                        cmd.CommandType = System.Data.CommandType.StoredProcedure;
+
+                        SqlDataReader reader = cmd.ExecuteReader(System.Data.CommandBehavior.CloseConnection);
+                        while (reader.Read())
+                        {
+                            SupplementAddRequest viewModel = new SupplementAddRequest();
+                            int index = 0;
+                            viewModel.Id = reader.GetInt32(index++);
+                            viewModel.SupplementName = reader.GetString(index++);
+                            viewModel.Brand = reader.GetString(index++);
+                            viewModel.Description = reader.GetString(index++);
+                            viewModel.Price = reader.GetString(index++);
+                            result.Add(viewModel);
+                        }
+                        return result;
+                    }
+                }
+                else
+                {
+                    return null;
+                }
+            }
+        }
+
+        public void Delete(int id)
+        {
+            string connStr = System.Configuration.ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
+            using (SqlConnection conn = new SqlConnection(connStr))
+            {
+                conn.Open();
+                if (conn.State == System.Data.ConnectionState.Open)
+                {
+                    string sqlCmd = "Supplemnt_Delete";
+
+                    using (SqlCommand cmd = new SqlCommand(sqlCmd, conn))
+                    {
+                        cmd.CommandType = System.Data.CommandType.StoredProcedure;
+
+                        cmd.Parameters.AddWithValue("@Id", id);
+
+                        cmd.ExecuteNonQuery();
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ MySampleApplication/MySampleApplication/Controllers/Api/SupplementController.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using MySampleApplication.Models.Request;
+using MySampleApplication.Models.Responses;
+using MySampleApplication.Services;
+
+namespace MySampleApplication.Controllers.Api
+{
+    [RoutePrefix("api/supplement")]
+    public class SupplementController : ApiController
+    {
+        [Route,HttpPost,AllowAnonymous]
+        public HttpResponseMessage InsertSupplement(SupplementAddRequest model)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                     SupplementService svc = new SupplementService();
+                     int? id = svc.Insert(model);
+                     ItemResponse<int?> resp = new ItemResponse<int?>();
+                     resp.Item = id;
+
+                     return Request.CreateResponse(HttpStatusCode.OK, resp);
+                }
+                else
+                {
+                    return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ModelState);
+                }
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ex);
+            }
+        }
+
+        [Route("{Id:int}"), HttpPut,AllowAnonymous]
+        public HttpResponseMessage UpdateSupplement(SupplementAddRequest model)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    SupplementService svc = new SupplementService();
+                    svc.Update(model);
+                    SuccessResponse resp = new SuccessResponse();
+                    return Request.CreateResponse(HttpStatusCode.OK, resp);
+                }
+                else
+                {
+                    return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ModelState);
+                }
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ex);
+            }
+        }
+
+        [Route("{Id:int}"), HttpGet,AllowAnonymous]
+        public HttpResponseMessage GetById(int id)
+        {
+            try
+            {
+                SupplementService svc = new SupplementService();
+                ItemResponse<SupplementAddRequest> resp = new ItemResponse<SupplementAddRequest>();
+                resp.Item = svc.SelectById(id);
+
+                return Request.CreateResponse(HttpStatusCode.OK, resp);
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ex);
+            }
+        }
+
+        [Route,HttpGet,AllowAnonymous]
+        public HttpResponseMessage GetAll()
+        {
+            try
+            {
+                SupplementService svc = new SupplementService();
+                ItemsResponse<SupplementAddRequest> resp = new ItemsResponse<SupplementAddRequest>();
+
+                resp.Items = svc.SelectAll();
+                return Request.CreateResponse(HttpStatusCode.OK, resp);
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ex);
+            }
+        }
+
+        [Route("{Id:int}"),HttpDelete,AllowAnonymous]
+        public HttpResponseMessage Delete(int id)
+        {
+            try
+            {
+                SupplementService svc = new SupplementService();
+                svc.Delete(id);
+
+                SuccessResponse resp = new SuccessResponse();
+
+                return Request.CreateResponse(HttpStatusCode.OK, resp);
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ex);
+            }
+        }
+    }
+}
